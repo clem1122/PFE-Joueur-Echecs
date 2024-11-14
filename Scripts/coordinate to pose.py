b473d051941188fbc169cfb2208c8444b607c470
Clément P <penicaut.clement@gmail.com>
2024-11-14 20:29:45 +0100

    fonctionnal coord to pose

--- Scripts/coordinate to pose.py
+++ Scripts/coordinate to pose.py
@@ -3,24 +3,48 @@
 from pyniryo import *
 
 
-origin_pose = PoseObject(x=0.093, y = -0.173, z = 0.110, roll = 2.23, pitch = 1.38, yaw = 0.68)
-
-def coo_to_pose(x, y):
-    return origin_pose.copy_with_offsets(x_offset = -0.05 * x, y_offset = -0.05 * y)
-
-robot_ip = "10.10.10.10"
-
-observation_pose = PoseObject(x=0.0, y=-0.19, z=0.35, roll=-3.13, pitch=1.40, yaw = 1.56)
-
-origin_pose = PoseObject(x=0.093, y = -0.173, z = 0.110, roll = 2.23, pitch = 1.38, yaw = 0.68)
+robot_ip = "192.168.10.1"
 
 robot = NiryoRobot(robot_ip)
+
+origin_pose = PoseObject(x=0.180, y = 0.136, z = 0.100, roll = 3.09, pitch = 1.43, yaw = -3.10)
+
+
+def coo_to_pose(nb_row, nb_column, z):
+    # z=0,1,1.5,6 (low,middle,high, above)
+    #Departure from H8
+    return origin_pose.copy_with_offsets(x_offset = 0.04 * (nb_row -1), y_offset = -0.04 * (nb_column -1) , z_offset = 0.02 * z)
+
+def go_take_move_release(x1,y1,x2,y2,z_grasp_piece):
+    robot.move_to_home_pose()
+    robot.move_pose(coo_to_pose(x1, y1, 4))
+    robot.move_pose(coo_to_pose(x1, y1, z_grasp_piece))
+    robot.close_gripper(speed = 800)
+    robot.move_pose(coo_to_pose(x1, y1, 6))
+    robot.move_pose(coo_to_pose(x2, y2, 6))
+    robot.move_pose(coo_to_pose(x2, y2, z_grasp_piece))
+    robot.open_gripper(speed = 800)
+    robot.move_pose(coo_to_pose(x2, y2, 6))
+    robot.move_to_home_pose()
+
+#Voir les tajectory pour avoir mouvement plus fluide
+
+#observation_pose = PoseObject(x=0.0, y=-0.19, z=0.35, roll=-3.13, pitch=1.40, yaw = 1.56)
+
+
 robot.calibrate_auto()
-robot.update_tool()
+go_take_move_release(2,5,4,5,1.5)
+#robot.update_tool()
+#robot.clear_collision_detected()
 
-robot.move_pose(coo_to_pose(0, 0))
+#robot.move_pose(0.16, 0.136, 0.15, 3., 0.65, 0)
+
+#robot.move_pose(origin_pose.copy_with_offsets(x_offset = 0.04 * 2, y_offset = -0.04 * 4, z_offset = 0.0))
+#robot.grasp_with_tool()
 
 
-robot.say("Bonjour tout le monde", 1)
+
+
+
 
 robot.close_connection()
