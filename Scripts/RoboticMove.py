12dcb88c5942d6350d6594d1f0952181cb0d85cd
clem1122 <penicaut.clement@gmail.com>
2024-11-20 02:05:57 +0100

    Robotic Move & Complex Move, Chessboard dict generation, Valhalla prototype

--- Scripts/RoboticMove.py
+++ Scripts/RoboticMove.py
@@ -2,10 +2,26 @@ import PChess as pc
 
 class Space:
 	def __init__(self):
-		self.chessboard = {
-			"e2" : (190,200), 
-			"e4" : (180,200)
-		}
+		self.chessboard = self.generate()
+		
+	def generate(self):
+		square_size = 5
+		columns = "abcdefgh"
+		rows = range(1, 9)
+		chessboard = {}
+		for i, column in enumerate(columns):
+			for j, row in enumerate(rows):
+				square_name = f"{column}{row}"
+				x = i * square_size
+				y = j * square_size
+				chessboard[square_name] = (x, y)
+				
+		for i in range(1, 9):
+			square_name = "v" + str(i)
+			chessboard[square_name] = (-10, i * square_size)
+		
+		return chessboard
+
 
 class RoboticMove:
 	def __init__(self, A, B):
@@ -22,15 +38,29 @@ def create_robotic_move(move):
 
 
 
-def complex_robotic_move_conversion(move):
+def create_complex_robotic_move(move):
+	A = move.start()
+	B = move.end()
+	if(move.isCapture() and move.isPromotion()):
+		raise Exception("Promotin + Capture")
+		
 	if(move.isCapture()):
-		pass
+		V = valhalla(A)
+		return [RoboticMove(B, V), RoboticMove(A, B)]
 	elif(move.isCastling()):
-		pass
+		C = "a1"
+		D = "a2"
+		return [RoboticMove(A, B), RoboticMove(C, D)]
 	elif(move.isPromotion()):
-		pass
+		F = valhalla(A)
+		V = valhalla(A)
+		return [RoboticMove(A, V), RoboticMove(F, B)]
 	elif(move.isEnPassant()):
-		pass
+		V = valhalla(A)
+		E = "a4";
+		return [RoboticMove(A, B), RoboticMove(E, V)]
 	else:
 		raise Exception("Move is not complex")
-	
+
+def valhalla(A):
+	return "v1"
