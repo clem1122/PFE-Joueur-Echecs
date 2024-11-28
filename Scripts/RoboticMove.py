6e610ff5d2cad89c897c90d4661804353a2e06da
Clément P <penicaut.clement@gmail.com>
2024-11-28 10:55:55 +0100

    Chessboard Generation

--- Scripts/RoboticMove.py
+++ Scripts/RoboticMove.py
@@ -1,20 +1,23 @@
 import PChess as pc
 
+
 class Space:
 	def __init__(self):
 		self.chessboard = self.generate()
+		self.a1_pose = [0.150, 0.136, 0.100]
 		
 	def generate(self):
-		square_size = 5
+		square_size = 4
 		columns = "abcdefgh"
 		rows = range(1, 9)
 		chessboard = {}
 		for i, column in enumerate(columns):
 			for j, row in enumerate(rows):
 				square_name = f"{column}{row}"
-				x = i * square_size
-				y = j * square_size
-				chessboard[square_name] = (x, y)
+				x = a1_pose[0] + i * square_size
+				y = a1_pose[1] -j * square_size
+				z = a1_pose[2]
+				chessboard[square_name] = (x, y, z)
 				
 		for i in range(1, 9):
 			b_square_name_1 = "v" + str(i)
