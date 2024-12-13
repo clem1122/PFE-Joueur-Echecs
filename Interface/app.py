6ad8214184394ce25917eb36f8363d63d9953371
clem1122 <penicaut.clement@gmail.com>
2024-12-13 08:12:22 +0100

    Flask bindings

--- Interface/app.py
+++ Interface/app.py
@@ -6,22 +6,29 @@ app = Flask(__name__)
 CORS(app)
 # Exemple de données à renvoyer
 data = {
-    "toggle1": "Information pour le bouton 1",
-    "toggle2": "Information pour le bouton 2",
-    "toggle3": "Information pour le bouton 3",
-    "toggle4": "Information pour le bouton 4"
+    "toggle1": "..................................1.1.................1.........",
+    "toggle2": "Coups possibles",
+    "toggle3": "Cases controlées",
+    "toggle4": "Aide"
 }
 
 @app.route('/get-info/<toggle_id>', methods=['GET'])
 def get_info(toggle_id):
+    print(toggle_id)
     # Retourne les infos pour un bouton spécifique
     if toggle_id in data:
         print(f"Returning info for {toggle_id}: {data[toggle_id]}")
-        result = jsonify({"info": data[toggle_id]})  # Debug
+        result = jsonify({"FEN": data[toggle_id]})  # Debug
         print("JSON : ", result)
         return result
     else:
         return jsonify({"error": "Bouton non trouvé"}), 404
 
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
--- Interface/sketch.js
+++ Interface/sketch.js
@@ -14,71 +14,90 @@ let Box;
 
 
 function preload() {
-  pieceImages['Q'] = loadImage("Images/Q.png");
-  pieceImages['K'] = loadImage("Images/K.png");
-  pieceImages['P'] = loadImage("Images/P.png");
-  pieceImages['N'] = loadImage("Images/N.png");
-  pieceImages['B'] = loadImage("Images/B.png");
-  pieceImages['R'] = loadImage("Images/R.png");
-  
-  pieceImages['q'] = loadImage("Images/q.png");
-  pieceImages['k'] = loadImage("Images/k.png");
-  pieceImages['p'] = loadImage("Images/p.png");
-  pieceImages['n'] = loadImage("Images/n.png");
-  pieceImages['b'] = loadImage("Images/b.png");
-  pieceImages['r'] = loadImage("Images/r.png");
+	pieceImages['Q'] = loadImage("Images/Q.png");
+	pieceImages['K'] = loadImage("Images/K.png");
+	pieceImages['P'] = loadImage("Images/P.png");
+	pieceImages['N'] = loadImage("Images/N.png");
+	pieceImages['B'] = loadImage("Images/B.png");
+	pieceImages['R'] = loadImage("Images/R.png");
+
+	pieceImages['q'] = loadImage("Images/q.png");
+	pieceImages['k'] = loadImage("Images/k.png");
+	pieceImages['p'] = loadImage("Images/p.png");
+	pieceImages['n'] = loadImage("Images/n.png");
+	pieceImages['b'] = loadImage("Images/b.png");
+	pieceImages['r'] = loadImage("Images/r.png");
 }
 
 function setup() {
 
-    const canvas = createCanvas(600, 600);
+	const canvas = createCanvas(600, 600);
 	canvas.parent('chessboard');
-    squareSize = width / rows;
-    for (let key in pieceImages) {
-    	pieceImages[key].resize(squareSize, 0);
-    }
-    
-    noStroke();
-    frameRate(10);
+	squareSize = width / rows;
+	for (let key in pieceImages) {
+		pieceImages[key].resize(squareSize, 0);
+	}
+
+	noStroke();
+	frameRate(10);
 }
 
 function draw() {
-    drawBoard();
+	drawBoard();
+	drawThreats();
+}
+
+function drawThreats() {
+	let threats = localStorage.getItem("ThreatsFEN");
+	if (threats === undefined) { return; }
+	console.log(threats[0]);
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			let x = j * squareSize;
+			let y = i * squareSize;
+			fill(100, 0, 0);
+			if (threats[i + j * 8] == '1') {
+				rect(x, y, squareSize, squareSize);
+			}
+
+		}
+	}
+
 }
 
 function drawBoard() {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            let x = j * squareSize;
-            let y = i * squareSize;
-            fill(colors[(i + j) % 2]);
-            if (selectedSquare != null) {
-		        let {row, col} = selectedSquare;
-		        if(row == i && col == j) {
-			        
-		        	fill(selectedColor)
-		        }
-            }
-            rect(x, y, squareSize, squareSize);
-            
-        }
-    }
-    drawPieces();
-}
-
-function drawPieces(){
 	for (let i = 0; i < rows; i++) {
 		for (let j = 0; j < cols; j++) {
-				let FENindex = isWhite ? i * 8 + j : 63 - (i * 8 + j);
-				if (FEN[FENindex] != '.') {
-					let x = j * squareSize;
-		           	let y = i * squareSize;
-					image(pieceImages[FEN[FENindex]], x, y);
+			let x = j * squareSize;
+			let y = i * squareSize;
+			fill(colors[(i + j) % 2]);
+			if (selectedSquare != null) {
+				let { row, col } = selectedSquare;
+				if (row == i && col == j) {
+
+					fill(selectedColor)
 				}
-		    }
+			}
+			rect(x, y, squareSize, squareSize);
+
 		}
+	}
+	drawPieces();
 }
- 
+
+function drawPieces() {
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			let FENindex = isWhite ? i * 8 + j : 63 - (i * 8 + j);
+			if (FEN[FENindex] != '.') {
+				let x = j * squareSize;
+				let y = i * squareSize;
+				image(pieceImages[FEN[FENindex]], x, y);
+			}
+		}
+	}
+}
+
 
 
 
@@ -86,39 +105,38 @@ function drawPieces(){
 function mousePressed() {
 
 
-	
+
 	let _col = floor(mouseX / squareSize);
 	let _row = floor(mouseY / squareSize);
 
 	if (_col >= 0 && _col < 8 && _row >= 0 && _row < 8) {
-	
+
 		let FENindex = isWhite ? _row * 8 + _col : 63 - (_row * 8 + _col);
-		if(selectedPiece == null) {
+		if (selectedPiece == null) {
 			if (FEN[FENindex] != '.') {
 				selectedPiece = FEN[FENindex];
 				console.log(selectedPiece);
 			}
 			selectedSquare = { row: _row, col: _col };
 		} else {
-		
-			
-			let endCoord = isWhite ? String.fromCharCode(97 + _col) + (8 - _row) : String.fromCharCode(104 - _col) + (1+_row);
-			
-			let {row, col} = selectedSquare;
-			
-			let startCoord = isWhite ? String.fromCharCode(97 + col) + (8 - row) : String.fromCharCode(104 - col) + (1+row);
+
+
+			let endCoord = isWhite ? String.fromCharCode(97 + _col) + (8 - _row) : String.fromCharCode(104 - _col) + (1 + _row);
+
+			let { row, col } = selectedSquare;
+
+			let startCoord = isWhite ? String.fromCharCode(97 + col) + (8 - row) : String.fromCharCode(104 - col) + (1 + row);
 			let move = startCoord + endCoord;
-			
+
 			selectedPiece = null;
 			selectedSquare = null;
-			sendData(move);
-			
+
 		}
-		
-		
-		
-		
-    	drawBoard();
+
+
+
+
+		drawBoard();
 	}
 }
 
