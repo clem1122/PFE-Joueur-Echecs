ba673db3f6c3728ed4601145bbf13454109a7c18
lucap <luca.perrin@ryerson.ca>
2025-01-07 16:42:57 +0100

    update

--- Vision/Python/image_processing.py
+++ Vision/Python/image_processing.py
@@ -10,48 +10,9 @@ def detect_differences(img1, img2, sensitivity_threshold):
     _, filtered_diff = cv2.threshold(diff, sensitivity_threshold, 255, cv2.THRESH_BINARY)
     return filtered_diff
 
-def determine_movement(modified_cases, rectified_img1, rectified_img2, rectified_reference, cases, sensitivity_threshold):
-    start_case, end_case = '', ''
-
-    for case in modified_cases:
-        coords = cases[case]
-        x_start, x_end, y_start, y_end = coords
-
-        # Extraire les régions d'intérêt
-        ref_square = rectified_reference[y_start:y_end, x_start:x_end]
-        prev_square = rectified_img1[y_start:y_end, x_start:x_end]
-        curr_square = rectified_img2[y_start:y_end, x_start:x_end]
-
-        # Calculer les différences par rapport à l'image de référence
-        diff_prev = cv2.absdiff(prev_square, ref_square)
-        diff_curr = cv2.absdiff(curr_square, ref_square)
-
-        diff_prev_count = np.sum(diff_prev > sensitivity_threshold)
-        diff_curr_count = np.sum(diff_curr > sensitivity_threshold)
-
-        # Déterminer les cases de départ et d'arrivée
-        if diff_prev_count > diff_curr_count:
-            start_case = case
-        elif diff_prev_count < diff_curr_count:
-            end_case = case
-
-    if start_case and end_case:
-        # Vérifier si c'était un mouvement simple ou une capture
-        arrival_prev_square = rectified_img1[cases[end_case][2]:cases[end_case][3], cases[end_case][0]:cases[end_case][1]]
-        reference_square = rectified_reference[cases[end_case][2]:cases[end_case][3], cases[end_case][0]:cases[end_case][1]]
-        diff_prev = cv2.absdiff(arrival_prev_square, reference_square)
-        diff_prev_count = np.sum(diff_prev > sensitivity_threshold)
-
-        if diff_prev_count < 250:
-            print(f"Mouvement simple détecté: {start_case} -> {end_case}")
-        else:
-            print(f"Capture détectée: {start_case} -> {end_case}")
-    else:
-        print("Aucun mouvement détecté.")
-
-
 # Analyse repere cases modifiees
 def analyze_squares(filtered_diff, cases, percentage_threshold, square_size):
+
     modified_cases = [] # Liste vide
 
     # Parcourir les cases du dictionnaire cases
@@ -69,3 +30,52 @@ def analyze_squares(filtered_diff, cases, percentage_threshold, square_size):
         if percentage_diff > percentage_threshold:
             modified_cases.append(case_name)
     return modified_cases
+
+# Determiner le mouvement effectue
+def determine_movement(modified_cases, rectified_img1, rectified_img2, rectified_reference, cases, sensitivity_threshold):
+    start_case, end_case = '', ''
+
+    # Parcours les cases modifees
+    for case in modified_cases:
+        #Extraction coords
+        coords = cases[case]
+        x_start, x_end, y_start, y_end = coords
+
+        # Extraire les régions d'intérêt de img_ref et img i, i+1
+        ref_square = rectified_reference[y_start:y_end, x_start:x_end]
+        prev_square = rectified_img1[y_start:y_end, x_start:x_end]
+        curr_square = rectified_img2[y_start:y_end, x_start:x_end]
+
+        # Calculer les différences de imag i, i+1 par rapport à img_ref
+        diff_prev = cv2.absdiff(prev_square, ref_square)
+        diff_curr = cv2.absdiff(curr_square, ref_square)
+
+        diff_prev_count = np.sum(diff_prev > sensitivity_threshold)
+        diff_curr_count = np.sum(diff_curr > sensitivity_threshold)
+
+        # Déterminer les cases de départ et d'arrivée
+        # 'si l'image precedent a - de diff que image actuelle, la piece est partie de cette case'
+        if diff_prev_count > diff_curr_count:
+            start_case = case
+        elif diff_prev_count < diff_curr_count:
+            end_case = case
+
+    if start_case and end_case:
+        # Vérifier si c'était un mouvement simple ou une capture
+        #extraction zones d'intteret
+        arrival_prev_square = rectified_img1[cases[end_case][2]:cases[end_case][3], cases[end_case][0]:cases[end_case][1]]
+        reference_square = rectified_reference[cases[end_case][2]:cases[end_case][3], cases[end_case][0]:cases[end_case][1]]
+        # Calcul diff de pixels 
+        diff_prev = cv2.absdiff(arrival_prev_square, reference_square)
+        diff_prev_count = np.sum(diff_prev > sensitivity_threshold)
+
+        #Max est 100x100=10,000
+        if diff_prev_count < 250:
+            move_type = "simple"
+            #print(f"Mouvement simple détecté: {start_case} -> {end_case}")
+        else:
+            move_type = "capture"
+            #print(f"Capture détectée: {start_case} -> {end_case}")
+    else:
+        move_type = "error"
+        #print("Aucun mouvement détecté.")
--- Vision/Python/main.py
+++ Vision/Python/main.py
@@ -4,23 +4,26 @@
 import cv2
 import os
 import numpy as np
-from calibration_utils import calibrate_corners, compute_transformation, rectify_image
+from calibration import calibrate_corners, compute_transformation, rectify_image
 from image_processing import detect_differences, analyze_squares, determine_movement
 
 # Paramètres
 calibration_file = "chessboard_calibration.pkl"
 reference_image_path = "img0.png"
-output_size = (800, 800) # Modifier et voir la difference (400,400)
+output_size = (800, 800) 
 square_size = output_size[0] // 8
 sensitivity_threshold = 40 # Seuil pour la diff de pixels 
 percentage_threshold = 22 # Seuil de pourcentage de diff pour considerer une case comme modifiee
 
+reference_image = cv2.imread("reference_image_path")
+print(reference_image.height, reference_image.width)
+cv2.imshow('reference_image', reference_image)
+
 ## Redressement de l'image de ref
-# Utilisation des fonctions de calibration_utils
+# Utilisation des fonctions de calibration
 input_points = calibrate_corners(calibration_file, reference_image_path, output_size)
 tform = compute_transformation(input_points, output_size)
-# Charger et redresser l'image de référence
-reference_image = cv2.imread(reference_image_path)
+# Redresser l'image de référence
 rectified_reference = rectify_image(reference_image, tform, output_size)
 
 # Creation du dictionnaire des coordonnées des cases
@@ -36,14 +39,16 @@ for row in range(8):
 
 # Chargement des images
 image_folder = "photos"
+# Trei par ordre numeiruque les fichiers .png
 image_files = sorted([f for f in os.listdir(image_folder) if f.endswith(".png")])
 
-# Main boucle 
+# MAIN LOOP 
 for i in range(len(image_files) - 1):
     # Charger les images en niveau de gris
     img1 = cv2.imread(os.path.join(image_folder, image_files[i]), cv2.IMREAD_GRAYSCALE)
     img2 = cv2.imread(os.path.join(image_folder, image_files[i + 1]), cv2.IMREAD_GRAYSCALE)
     #Redresser les images en utilisant l'image ref 
+    # On utilise le meme tform que pour l'image de calibration
     rectified_img1 = rectify_image(img1, tform, output_size)
     rectified_img2 = rectify_image(img2, tform, output_size)
     # Calculer les diff en utilisant la fonction de image_processing
@@ -61,3 +66,8 @@ for i in range(len(image_files) - 1):
             percentage_diff = int((diff_pixels / total_pixels) * 100)
             print(f"Case {case}:{percentage_diff:.2f}%")
 
+        # Movment simple ou capture ?
+        movement = determine_movement(modified_cases, rectified_img1, rectified_img1, rectified_reference, cases, sensitivity_threshold)
+
+        # print le move 
+        print(f"Mouvement: {movement['move_type']} de {movement['start_case']} a {movement['end_case']}")
