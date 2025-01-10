898d37d69c982d666d6e525cdded29cad82069ee
Gaëlle Dumont Le Brazidec <gaelledumontlb@gmail.com>
2025-01-10 04:45:08 +0100

    modif value loss

--- IA/model/Loss.py
+++ IA/model/Loss.py
@@ -12,24 +12,30 @@ class PolicyLoss(nn.Module):
         return loss
     
 class ValueLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, gamma=1.0):
         super(ValueLoss, self).__init__()
         self.MSE = nn.MSELoss()
+        self.gamma = gamma
 
-    def forward(self, preds, targets):
+    def forward(self, preds, targets, num_moves):
+        T = targets.size(0)
+        weight = 1 / (1 + torch.exp(-self.gamma * (num_moves - T / 2)))
+
         loss = self.MSE(preds, targets)
 
-        return loss
+        weighted_loss = weight * loss
+        
+        return weighted_loss.mean()
     
 class Loss():
-    def __init__(self, alpha=1.0, beta=1.0):
+    def __init__(self, alpha=1.0, beta=1.0, gamma=1.0):
         self.alpha = alpha
         self.beta = beta
         self.PolicyLoss = PolicyLoss()
-        self.ValueLoss = ValueLoss()
+        self.ValueLoss = ValueLoss(gamma)
 
-    def forward(self, preds_pol, targets_pol, preds_val, targets_val):
+    def forward(self, preds_pol, targets_pol, preds_val, targets_val, num_moves):
         pol_loss = self.PolicyLoss(preds_pol, targets_pol)
-        val_loss = self.ValueLoss(preds_val, targets_val)
+        val_loss = self.ValueLoss(preds_val, targets_val, num_moves)
 
         return self.alpha * pol_loss + self.beta * val_loss
