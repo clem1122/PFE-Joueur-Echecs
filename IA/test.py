3131c24f7c5f23cb0373785060cf38e85811027b
Gaëlle Dumont Le Brazidec <gaelledumontlb@gmail.com>
2025-01-15 10:44:52 +0100

    update

--- IA/test.py
+++ IA/test.py
@@ -14,12 +14,30 @@ cfgs = os.path.join(CONFIGROOT, learning_configs)
 with open(cfgs, "r") as f:
     config = yaml.safe_load(f)
 
-def test_model(model_path, test_dataset, mapping):
-    """Test the loaded model on a sample dataset."""
+
+def accuracy(outputs, targets, value_preds, value_targets):
+
+        _, predicted_indices = torch.max(outputs, 1)  # Shape: [batch_size]
+        _, predicted_values = torch.max(value_preds, 1)
+    
+        true_indices = torch.argmax(targets, 1)  # Shape: [batch_size]
+        true_values = torch.argmax(value_targets, 1) 
+        
+        correct = (predicted_indices == true_indices).sum().item()
+        correct_values = (predicted_values == true_values).sum().item()
+        
+        total = targets.size(0)
+        total_values = value_targets.size(0)
+       
+        return correct / total, correct_values / total_values
+
+def test_model(model_path, test_dataset, mapping, config):
 
     if not os.path.exists(model_path):
         print(f"Model file {model_path} does not exist.")
         return
+    
+    config = config["train"]["tune"]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = Model(
@@ -44,7 +62,7 @@ def test_model(model_path, test_dataset, mapping):
     # Prepare the test data loader
     test_loader = DataLoader(
         ChessDataset(test_dataset, mapping, fraction=0.05),
-        batch_size=8,  # Example value; adjust as needed
+        batch_size=32, 
         collate_fn=collate_fn,
         shuffle=False
     )
@@ -54,22 +72,21 @@ def test_model(model_path, test_dataset, mapping):
         for i, (board_tensors, target_vectors, value_targets, move_indices, tot_moves) in enumerate(test_loader):
             board_tensors = board_tensors.float().to(device)
 
-            # Forward pass
             outputs, value_preds = model(board_tensors)
 
             print(f"Batch {i + 1}:")
             print(f"Predicted Outputs Shape: {outputs.shape}")
             print(f"Predicted Value Outputs Shape: {value_preds.shape}")
+            vec_acc, val_acc = accuracy(outputs, target_vectors, value_preds, value_targets)
 
-            # Only process the first batch for testing
+            print("accuracy", vec_acc, val_acc)
+
             break
 
 if __name__ == "__main__":
-    model_path = "model.pth"  # Path to the saved model
+    model_path = "model_checkpoint.pth" 
 
-    # Load the test dataset (replace with actual dataset path or loader)
     from datasets import load_dataset
     test_dataset = load_dataset('angeluriot/chess_games')
 
-    # Perform the test
-    test_model(model_path, test_dataset, new_mapping)
+    test_model(model_path, test_dataset, new_mapping, config)
