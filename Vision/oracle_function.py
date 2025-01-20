a1349a7ac42f32ff9cf647873da4bd1189de276c
lucap <luca.perrin@ryerson.ca>
2025-01-20 12:06:48 +0100

    //

--- Vision/oracle_function.py
+++ Vision/oracle_function.py
@@ -19,7 +19,7 @@ def oracle(img1,img2, reference_image, debug = False):
 
     # ------------- PARAMETERS -------------------
     threshold_diff = 50 #dans 'detect_difference' : Seuil pour la diff de pixels 
-    threshold_empty = 40 #dans 'is square_empty': Seuil pour diff entre case et case empty
+    threshold_empty = 20 #dans 'is square_empty': Seuil pour diff entre case et case empty
     
     # ----------------------------------------------------------------------------------------------
     calibration_file = "chessboard_calibration.pkl"
