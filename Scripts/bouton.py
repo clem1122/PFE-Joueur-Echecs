303d9af6b8dc493d2b10a6f01219e9bfc69f78f6
Clément P <penicaut.clement@gmail.com>
2025-01-27 17:24:06 +0100

    boutton test2

--- Scripts/bouton.py
+++ Scripts/bouton.py
@@ -5,12 +5,8 @@ import time
 handle = lgpio.gpiochip_open(0)  # Ouvre le premier GPIO chip
 
 # Définir GPIO 2 comme une entrée (pour le bouton)
-button_pin = 17
+button_pin = 4
 lgpio.gpio_claim_input(handle, button_pin)
-
-# Définir GPIO 17 comme une sortie (pour la LED)
-led_pin = 27
-lgpio.gpio_claim_output(handle, led_pin)
 
 # Boucle infinie pour détecter l'appui sur le bouton
 try:
@@ -19,14 +15,8 @@ try:
         button_state = lgpio.gpio_read(handle, button_pin)
         
         # Si le bouton est pressé (l'état est 0 pour un bouton poussoir avec pull-up)
-        if button_state == 1:
-            print("Bouton appuyé!")
-            # Allumer la LED
-            lgpio.gpio_write(handle, led_pin, 1)
-        else:
-            # Éteindre la LED si le bouton n'est pas pressé
-            lgpio.gpio_write(handle, led_pin, 0)
-        
+        if button_state == 0:
+            print("Bouton appuyé")
         # Attendre un court instant pour ne pas surcharger le processeur
         time.sleep(0.1)
 
